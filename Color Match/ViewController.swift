cdfbde97a261f218bd0c51723690216332be2c0f
Matthew Schmale <Matt@Matthews-MacBook-Pro-2.local>
2016-11-30 16:35:51 -0500

    Add VC and test labels

--- Color Match/ViewController.swift
+++ Color Match/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var highScoreLbl: UILabel!
+
+    @IBAction func onBtnPress(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
