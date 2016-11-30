843fdb73a7a9bf6f6a0f620a7d093f1c62b2e187
Matthew Schmale <Matt@Matthews-MacBook-Pro-2.local>
2016-11-30 16:53:35 -0500

    Added new VC with .swift file

---
+++ Color Match/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  Color Match
+//
+//  Created by Matthew Schmale on 11/30/16.
+//  Copyright © 2016 Schmale Cunha. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    
+
+}
