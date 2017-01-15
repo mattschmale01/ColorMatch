005598afdb5da4d16fb4d74f7af41873f1a14d97
Peter Cunha <petercunha8@gmail.com>
2017-01-15 15:55:44 -0500

    Complete animation sequence

--- Color Match/SettingsPopUpViewController.swift
+++ Color Match/SettingsPopUpViewController.swift
@@ -12,12 +12,13 @@ class SettingsPopUpViewController: UIViewController {
 
     // Close popUp
     @IBAction func closePopUp(_ sender: Any) {
-        self.view.removeFromSuperview()
+        self.removeAnimate()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.showAnimate()
         // Do any additional setup after loading the view.
     }
 
@@ -26,15 +27,26 @@ class SettingsPopUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
     }
-    */
-
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion: {
+            (finished : Bool)  in
+            if (finished) {
+                self.view.removeFromSuperview()
+            }
+        })
+    }
 }
