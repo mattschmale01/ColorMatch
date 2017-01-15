2d52719693273d0c1aaffd5df5d86143e28dda4f
Matthew Schmale <Matt@pal-nat186-103-239.itap.purdue.edu>
2017-01-15 18:22:32 -0500

    More restart VC

---
+++ Color Match/restartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  restartViewController.swift
+//  Color Match
+//
+//  Created by Matthew Schmale on 1/15/17.
+//  Copyright © 2017 Schmale Cunha. All rights reserved.
+//
+
+import UIKit
+
+class restartViewController: UIViewController {
+    @IBOutlet weak var
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction onCloseBtnPress(sender: any) {
+    
+    }
+
+}
