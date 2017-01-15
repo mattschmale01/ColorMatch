426e1adaaa89727ac302f34929937eda5980a1ab
Matthew Schmale <Matt@pal-nat186-103-239.itap.purdue.edu>
2017-01-15 15:51:11 -0500

    Pop ups\!

--- Color Match/helpViewPopUpController.swift
+++ Color Match/helpViewPopUpController.swift
@@ -9,32 +9,40 @@
 import UIKit
 
 class helpViewPopUpController: UIViewController {
-
+    
+    //Func: Closes Help Screen
+    
     @IBAction func onCloseBtnPress(_ sender: Any) {
-        
         self.view.removeFromSuperview()
+        removeAnimate()
     }
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        showAnimate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
     }
-    */
-
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
 }
